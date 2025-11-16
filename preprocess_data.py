26ace8330b3119ee1ad9e0fd0c545922f885ba06
Ali_Bix <alice.benoist@esme.fr>
2025-11-16 16:30:32 +0100

    changed the function name

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 
-def preprocess_data(data, testSize):
+def a_very_ccol_function(data, testSize):
     """Nettoie, met en forme les données et prépare les ensembles de train et 
     de test"""
     train, test = train_test_split(data, test_size = testSize)
